c4eca2d8a940b43ac1ab4bfe0be70080fa373c53
𝙄𝙁𝙀𝘼𝙉𝙔𝙄 𝙉𝙉𝘼𝙈𝘼𝙉𝙄 ♿💫 <Ifeanyinuels32@outlook.com>
2026-03-18 19:23:33 +0100

    Refactor scroll and addToCartWA functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Scroll to section
+// Scroll smooth
 function scrollToSection(id){
   document.getElementById(id).scrollIntoView({behavior:"smooth"});
 }
@@ -8,15 +8,13 @@ function addToCartWA(name, price, btn){
   btn.innerText = "Added ✅";
   btn.disabled = true;
 
-  const phone = "2349019071679"; // Your WhatsApp number with country code
+  const phone = "2349019071679";
   const text = `Hello, I WANT TO BUY ${name} FOR ₦${price}`;
   const waLink = `https://wa.me/${phone}?text=${encodeURIComponent(text)}`;
+  window.open(waLink,"_blank");
 
-  window.open(waLink, "_blank");
-
-  // Reset button after 5 seconds
   setTimeout(()=>{
     btn.innerText = "Add to Cart";
     btn.disabled = false;
-  },5000);
+  },4000);
 }
